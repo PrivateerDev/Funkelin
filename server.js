826dcd9e48be70c88d272bd43b74c59cf02d77cd
Marlene Ortega <marlene.ortega@estudiante.uacm.edu.mx>
2025-04-17 22:26:02 -0600

    Reorganizar estructura del proyecto Funkelin

--- server.js
+++
@@ -1,4 +0,0 @@
-const app = express(); 
-app.get('/', (req, res) = Backend Funcionando!')); 
-app.listen(3000, () = Funkelin activo en el puerto 3000')); 
-app.use('/api/registroUsuario', registroUsuario); 
